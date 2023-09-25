11ffaeae3c2af66cc56f9a874a17c6f25dd5ed78
szt-rookiecoder <szt0218@hotmail.com>
2023-09-25 23:14:40 +0800

    higher order functions

--- functions/hw.py
+++ functions/hw.py
@@ -62,5 +62,43 @@ def hailstone(n):
     return length
 
 
+"""higher order functions and lambda expressions"""
+
+def compose1(f, g):
+    """Return the composition function which given x, computes f(g(x)).
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2
+    >>> a1 = compose1(square, add_one)   # (x + 1)^2
+    >>> a1(4)
+    25
+    >>> mul_three = lambda x: x * 3      # multiplies 3 to x
+    >>> a2 = compose1(mul_three, a1)    # ((x + 1)^2) * 3
+    >>> a2(4)
+    75
+    >>> a2(5)
+    108
+    """
+    return lambda x: f(g(x))
+
+def composite_identity(f, g):
+    """
+    Return a function with one parameter x that returns True if f(g(x)) is
+    equal to g(f(x)). You can assume the result of g(x) is a valid input for f
+    and vice versa.
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2
+    >>> b1 = composite_identity(square, add_one)
+    >>> b1(0)                            # (0 + 1)^2 == 0^2 + 1
+    True
+    >>> b1(4)                            # (4 + 1)^2 != 4^2 + 1
+    False
+    """
+    def function(x):
+        return f(g(x)) == g(f(x))
+    return function
+
+
 
 
